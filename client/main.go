60c7bc3b52f7ca7da4a547acdc663317e3f4ab63
aerth <aerth@riseup.net>
2017-07-04 23:13:50 -0700

    lerp camera towards player

--- client/main.go
+++ client/main.go
@@ -177,6 +177,7 @@ func run(protocol, addr, id string) error {
 			log.Printf("Non-fatal Error: %v", err)
 		}
 	}()
+	camPos := pixel.ZV
 	playerText := text.New(pixel.ZV, atlas)
 	for !win.Closed() {
 		win.Clear(colornames.Yellow)
@@ -196,8 +197,11 @@ func run(protocol, addr, id string) error {
 		lootSprite.Draw(win, pixel.IM.Scaled(pixel.ZV, 2.0))
 		g.lock.RLock()
 		pos := g.players[id].Position
+		camPos = pixel.Lerp(camPos, g.wincenter.Sub(pos), 1-math.Pow(1.0/128, dt))
+		cam := pixel.IM.Moved(camPos)
+		win.SetMatrix(cam)
 		for _, player := range g.players {
-			playerPos := pixel.IM.Moved(pixel.V(player.Position.X, player.Position.Y))
+			playerPos := pixel.IM.Moved(player.Position)
 			playerSprite.Draw(win, playerPos, player.Color)
 			g.speechLock.RLock()
 			txt, ok := g.playerSpeech[player.ID]
@@ -217,26 +221,23 @@ func run(protocol, addr, id string) error {
 
 			if g.speechMode && id == player.ID {
 				playerText.Clear()
-				playerText := text.New(pixel.ZV, atlas)
 				playerText.Dot = playerText.Orig
 				playerText.Dot.X -= playerText.BoundsOf(g.currentSpeechBuffer+"_").W() / 2
 				playerText.WriteString(g.currentSpeechBuffer + "_")
 				playerText.DrawColorMask(win,
-					pixel.IM.Scaled(pixel.ZV, 2).Moved(pixel.V(player.Position.X, player.Position.Y+40)),
+					pixel.IM.Scaled(pixel.ZV, 2).Moved(pixel.V(player.Position.X, player.Position.Y-64)),
 					colornames.White)
 			}
 		}
 		g.lock.RUnlock()
 
-		cam := pixel.IM.Moved(g.wincenter.Sub(pixel.V(pos.X, pos.Y)))
-
-		playerText.Clear()
 		// show mouse coordinates
 		mousePos := cam.Unproject(win.MousePosition())
+		playerText.Clear()
+		playerText.Dot = playerText.Orig
 		playerText.WriteString(fmt.Sprintf("%v", mousePos))
-		playerText.DrawColorMask(win, pixel.IM.Moved(mousePos), colornames.Firebrick)
+		playerText.DrawColorMask(win, pixel.IM.Moved(mousePos), colornames.White)
 
-		win.SetMatrix(cam)
 		win.Update()
 
 		fps++
@@ -382,7 +383,7 @@ func (g *GameWorld) processPlayerInput(conn net.Conn, win *pixelgl.Window) error
 	}
 	if mousedir != shared.DIR_NONE {
 		g.queueSimulation(func() {
-			g.setPlayerPosition(g.playerID, g.players[g.playerID].Position.Add(mousedir.ToVec().Scaled(2)))
+			g.setPlayerPosition(g.playerID, g.players[g.playerID].Position.Add(mousedir.ToVec()))
 		})
 		// set sprite facing
 		g.facing = mousedir
@@ -408,44 +409,28 @@ func (g *GameWorld) processPlayerInput(conn net.Conn, win *pixelgl.Window) error
 	if win.Pressed(pixelgl.KeyA) {
 		g.facing = LEFT
 		g.action = shared.A_WALK
-		g.queueSimulation(func() {
-			g.setPlayerPosition(g.playerID, g.players[g.playerID].Position.Add(LEFT.ToVec()))
-		})
-		if err := requestMove(LEFT, conn); err != nil {
-			return err
-		}
 	}
 	if win.Pressed(pixelgl.KeyD) {
 		g.facing = RIGHT
 		g.action = shared.A_WALK
-		g.queueSimulation(func() {
-			g.setPlayerPosition(g.playerID, g.players[g.playerID].Position.Add(RIGHT.ToVec()))
-		})
-		if err := requestMove(RIGHT, conn); err != nil {
-			return err
-		}
 	}
 	if win.Pressed(pixelgl.KeyW) {
 		g.facing = UP
 		g.action = shared.A_WALK
-		g.queueSimulation(func() {
-			g.setPlayerPosition(g.playerID, g.players[g.playerID].Position.Add(UP.ToVec()))
-		})
-		if err := requestMove(UP, conn); err != nil {
-			return err
-		}
 	}
 	if win.Pressed(pixelgl.KeyS) {
 		g.facing = DOWN
 		g.action = shared.A_WALK
-
+	}
+	if g.action == shared.A_WALK {
 		g.queueSimulation(func() {
-			g.setPlayerPosition(g.playerID, g.players[g.playerID].Position.Add(DOWN.ToVec()))
+			g.setPlayerPosition(g.playerID, g.players[g.playerID].Position.Add(g.facing.ToVec()))
 		})
-		if err := requestMove(DOWN, conn); err != nil {
+		if err := requestMove(g.facing, conn); err != nil {
 			return err
 		}
 	}
+
 	return nil
 }
 
