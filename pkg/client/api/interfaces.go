d4eea683467bc7d91f5f10fb9cc8d578a2234fa2
Scott Weiss <sdw35@cornell.edu>
2017-08-13 01:53:31 -0400

    remove unused

--- pkg/client/api/interfaces.go
+++ pkg/client/api/interfaces.go
@@ -1,17 +1,6 @@
 package api
 
-import (
-	"github.com/faiface/pixel"
-	"github.com/mmogo/mmo/pkg/shared"
-)
-
 // abstracts a common interface that can be run from cmd/client/main.go
 type Client interface {
 	Run()
-}
-
-// Renderer is responsible for rendering the state onto a target
-type Renderer interface {
-	// RenderFrame draws frame draws a single frame (within a refresh loop)
-	RenderFrame(target pixel.Target, world *shared.World)
 }
